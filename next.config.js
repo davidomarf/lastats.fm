e505de3b2c4b107cec49f0fa9a842b8fd74d12c8
davidomarf <david@davidomar.com>
2021-10-26 21:38:11 -0500

    Upgrade to next@12

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const path = require("path");
-const withSass = require("@zeit/next-sass");
 
-module.exports = withSass({
-  /* bydefault config  option Read For More Optios
-here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-  cssModules: true,
-});
+
 module.exports = {
   /* Add Your Scss File Folder Path Here */
   sassOptions: {
