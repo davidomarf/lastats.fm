55964e8ccbd5ee9083876da57eaf3e6930ceeb65
davidomarf <david@davidomar.com>
2022-08-21 01:49:35 -0500

    Add the lastfm API functions to request lastfm data

---
+++ src/api/lastfm.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { RecentTracks, Track } from "models/ScrobblePage";
+import { User } from "models/User";
+
+const lastFmAPI =
+  process.env.REACT_APP_LAST_FM_API || "ab50ffdf56122232fa11a39059f77a42";
+const lastFmCall =
+  "https://ws.audioscrobbler.com/2.0/?format=json&api_key=" + lastFmAPI;
+
+async function getData<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get<T>(url);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Returns the information of a Last.fm user
+ *
+ * @param  user Last.fm user
+ * @returns Response to the method https://www.last.fm/api/show/user.getInfo
+ */
+export async function getUserInfo(user: string): Promise<User> {
+  return (
+    (await getData<User>(
+      lastFmCall + "&method=user.getinfo&user=" + user
+    )) as any
+  ).user;
+}
+
+/**
+ * Returns the specified page from the user's scrobble list. Each page has 200 entries.
+ * @param {string} user
+ * @param {number} page Page of the
+ * @returns {Promise} Response to the method https://www.last.fm/api/show/user.getRecentTracks
+ */
+export async function getScrobbles(
+  user: string,
+  page: number,
+  limit: number = 200
+): Promise<Track[]> {
+  return (
+    (await getData<RecentTracks>(
+      `${lastFmCall}&method=user.getrecenttracks&limit=${limit}&user=${user}&page=${page}`
+    )) as RecentTracks
+  ).recenttracks.track;
+}
--- src/components/username/Username.tsx
+++ src/components/username/Username.tsx
@@ -1,13 +1,24 @@
+import { getUserInfo } from "api/lastfm";
 import classNames from "classnames/bind";
-import { useRef } from "react";
+import { FormEvent, useCallback, useRef, useState } from "react";
 import styles from './Username.module.scss';
 
 const cx = classNames.bind(styles);
 
 function UsernameInput() {
+  const [username, setUsername] = useState('');
+
+  const handleFormSubmit = useCallback((event: FormEvent) => {
+    event.preventDefault();
+
+    getUserInfo(username).then(console.log)
+  }, [username])
+
   const usernameInputRef = useRef<HTMLInputElement>(null);
   return (
-    <div className={cx("start-action-container")} onClick={() => usernameInputRef.current?.focus()}>
+    <div
+      className={cx("start-action-container")}
+      onClick={() => usernameInputRef.current?.focus()}>
       <div className={cx('start-action__title')}>
         Enter your username,
       </div>
@@ -15,8 +26,22 @@ function UsernameInput() {
         and the website will start to update every few seconds with
         all the new data it gets from your scrobbling history
       </div>
-      <label htmlFor="username" style={{ "display": "none" }}>Username</label>
-      <input ref={usernameInputRef} type="text" name="username" id="input-username" placeholder="Username" />
+      <form onSubmit={handleFormSubmit} action="">
+        <label
+          htmlFor="username"
+          style={{ "display": "none" }}
+        >
+          Username
+        </label>
+        <input ref={usernameInputRef}
+          type="text"
+          name="username"
+          id="input-username"
+          placeholder="Username"
+          onChange={event => setUsername(event.target.value)}
+        />
+        <button type="submit"> Go -> </button>
+      </form>
     </div>
   );
 }
